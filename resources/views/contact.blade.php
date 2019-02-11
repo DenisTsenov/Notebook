9472b3239277263e7a1ab16788585e34c2f4a05d
DenisTsenov <denis_cenov_@abv.bg>
2019-02-11 23:07:54 +0200

    add basic views and basic routes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -85,14 +85,9 @@
                 </div>
 
                 <div class="links">
-                    <h1>Start</h1>
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    Contact the creator
+                    <p>Email: test@test.com</p>
+                    <p>Phone: 0000 000 000</p>
                 </div>
             </div>
         </div>
