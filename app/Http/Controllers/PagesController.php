1d3e7b7ff5cc161a864ad09919b5eeee0721f9a4
DenisTsenov <denis_cenov_@abv.bg>
2019-03-19 00:06:48 +0200

    add default HTML for navigation bar

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,8 +3,7 @@
 namespace App\Http\Controllers;
 
 /**
- * Description of PagesController
- * This controller is responsible for all static pages
+ * This controller manage for all static pages
  *
  * @author denis
  */
