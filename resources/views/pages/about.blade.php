1fc51f6518df3e0c5c74f09a063484f3196c2c54
DenisTsenov <denis_cenov_@abv.bg>
2019-03-23 23:43:00 +0200

    separate main page content to partials

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,8 @@
 @section('title', 'About me')
 
 @section('content')
+
 <h3 class="text-center">Who am I?</h3>
 Hello my name is Denis... bla bla bla Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+
 @endsection
