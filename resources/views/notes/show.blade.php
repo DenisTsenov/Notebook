9e9aae360bcf7fc6805f07fe9bdc9218fee5f679
DenisTsenov <denis_cenov_@abv.bg>
2019-03-31 00:23:49 +0200

    add meta data into the Note view

--- resources/views/notes/show.blade.php
+++ resources/views/notes/show.blade.php
@@ -3,13 +3,38 @@
 @section('title', 'View Note')
 
 @section('content')
-    
-    @if(empty($note->title))
+<div class="row">
+    <div class="col-md-8">
+        @if(empty($note->title))
         <p class="h3 text-center">No title</p>>
-    @elseif($note->title)
+        @elseif($note->title)
         <p class="h3 text-center">{{ $note->title }}</p>
-    @endif
-    <hr/>
-    <p class="lead text-center">{{ $note->content }}</p>
+        @endif
+        <hr/>
+        <p class="lead text-center">{{ $note->content }}</p>
+    </div>
+    <div class="col-md-4">
+        <div class="well">
+            <dl class="dl-hroizontal">
+                <dt class="">Created at</dt>
+                <dd>{{ date("Y / M / d(D) - H:i", time($note->created_at)) }}</dd>
+            </dl>
 
+            <dl class="dl-hroizontal">
+                <dt>Last Updated at</dt>
+                <dd>{{ date("Y / M / d(D) - H:i", time($note->created_at)) }}</dd>
+                <hr/>
+                <div class="row">
+                    <div class="col-sm-6">
+                        {!! Html::linkRoute('note.edit', 'Edit', [$note->id], ['class' => 'btn btn-info btn-block']) !!}
+                    </div>
+                    <div class="col-sm-6">
+                        {!! Html::linkRoute('note.destroy', 'Delete', [$note->id], ['class' => 'btn btn-danger btn-block']) !!}
+                    </div>
+                </div>
+            </dl>
+        </div>
+    </div>
+
+</div>
 @endsection
